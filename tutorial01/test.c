0c5656fa5ea1ac6bb355ee2d55c4d4e0f2785de3
wuxiaoweisjz@126.com <wuxiaoweisjz@126.com>
2021-05-17 23:43:50 +0800

    实现单个解析false和true

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -7,6 +7,8 @@ static int main_ret = 0;
 static int test_count = 0;
 static int test_pass = 0;
 
+static void test_parse_false();
+
 #define EXPECT_EQ_BASE(equality, expect, actual, format) \
     do {\
         test_count++;\
@@ -27,6 +29,12 @@ static void test_parse_null() {
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
+static void test_parse_true() {
+    lept_value v;
+    v.type = LEPT_TRUE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
+    EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
+}
 static void test_parse_expect_value() {
     lept_value v;
 
@@ -62,6 +70,15 @@ static void test_parse() {
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
+    test_parse_true();
+    test_parse_false();
+}
+
+static void test_parse_false() {
+    lept_value v;
+    v.type = LEPT_TRUE;
+    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
+    EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
 
 int main() {
